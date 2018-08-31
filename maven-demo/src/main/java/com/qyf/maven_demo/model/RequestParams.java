3aaf868b641bb54c626a0449ce52139c5e2c0ba9
qyf <1085527496@qq.com>
2018-08-31 14:08:25 +0800

    添加参数校验

---
+++ maven-demo/src/main/java/com/qyf/maven_demo/model/RequestParams.java
@@ -0,0 +1,32 @@
+package com.qyf.maven_demo.model;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel()
+public class RequestParams implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@NotBlank(message="名字不能为空")//参数校验判空，适用于字符串
+	@ApiModelProperty(value="账号",example="114")
+	private String name;
+	@NotNull(message="年龄必传")
+	@ApiModelProperty(value="年龄",example="22")
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
--- maven-demo/src/main/java/com/qyf/maven_demo/web/TestController.java
+++ maven-demo/src/main/java/com/qyf/maven_demo/web/TestController.java
@@ -1,15 +1,33 @@
 package com.qyf.maven_demo.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.qyf.maven_demo.service.TestService;
+import com.qyf.maven_demo.model.RequestParams;
+import com.qyf.maven_demo.model.RespModel;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
-	@Autowired
-	private TestService serviceTest;
 	
+	/**
+	 * @author qyf
+	 * @param data
+	 * @return Object
+	 */
+	@PostMapping("valid")
+	@ApiOperation(value="参数校验")
+	@ApiResponses({@ApiResponse(code = 200, message = "返回值说明", response = RespModel.class)})
+	public Object getHistoryReportwork(@Valid @RequestBody @ApiParam(name="参数",value="传入json格式",required=true)  RequestParams params) {
+	     return null;
+	}
 }
