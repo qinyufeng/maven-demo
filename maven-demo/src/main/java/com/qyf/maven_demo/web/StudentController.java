a7a1f6339dd64dc6080d7b25618c125e7e48446e
qyf <1085527496@qq.com>
2018-10-31 09:44:24 +0800

    添加批量增删改查demo

--- maven-demo/src/main/java/com/qyf/maven_demo/web/StudentController.java
+++ maven-demo/src/main/java/com/qyf/maven_demo/web/StudentController.java
@@ -1,13 +1,20 @@
 package com.qyf.maven_demo.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.qyf.maven_demo.model.Student;
 import com.qyf.maven_demo.service.StudentService;
 
@@ -23,7 +30,7 @@ import com.qyf.maven_demo.service.StudentService;
 public class StudentController {
 	@Autowired
 	private StudentService service;
-	
+	/************************* 单表 常用的基本的增删改查 ***************************/
 	@PostMapping
 	private Student insert(Student stu) {
 		service.insert(stu);
@@ -41,5 +48,41 @@ public class StudentController {
 	@DeleteMapping
 	private boolean delete(int id) {
 		return service.deleteById(id);
+	}
+	/************************ 批量增删改查 ****************************/
+	/**
+	 * 批量插入
+	 */
+	@PostMapping("/insertList")
+	private List<Student> insertList(@RequestBody List<Student> paramList){
+		service.insertBatch(paramList);
+		return paramList;
+	}
+	/**
+	 * 批量插入更新，存在就更新，不存在就插入
+	 */
+	@PostMapping("insertOrUpdateList")
+	private List<Student> insertOrUpdateList(@RequestBody List<Student> paramList){
+		service.insertOrUpdateBatch(paramList);
+		return paramList;
+	}
+	/**
+	 * 批量更新
+	 */
+	@PutMapping("updateList")
+	private List<Student> updateList(@RequestBody List<Student> paramList){
+		service.updateBatchById(paramList);
+		return paramList;
+	}
+	/**
+	 * 查询
+	 */
+	@GetMapping("/getlist/{data}")
+	private List<Student> selectList(@PathVariable String data){
+		Student stuParam=JSON.parseObject(data, Student.class);
+		Wrapper<Student> wrapper=new EntityWrapper<>();
+		wrapper.like("name", stuParam.getName());
+		List<Student> stuList=service.selectList(wrapper);
+		return stuList;
 	}
 }
