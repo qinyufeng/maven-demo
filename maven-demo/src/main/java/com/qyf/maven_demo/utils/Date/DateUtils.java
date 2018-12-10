cd67af2d545845589ce99d12b204ed61df45513d
qyf <1085527496@qq.com>
2018-12-10 17:57:34 +0800

    update

--- maven-demo/src/main/java/com/qyf/maven_demo/utils/Date/DateUtils.java
+++ maven-demo/src/main/java/com/qyf/maven_demo/utils/Date/DateUtils.java
@@ -1,6 +1,12 @@
 package com.qyf.maven_demo.utils.Date;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public final class DateUtils {
 	/** 日期格式 **/
@@ -17,10 +23,11 @@ public final class DateUtils {
 		String YYMMDD = "yyMMdd";
 		String YY_MM_DD = "yy-MM-dd";
 	}
-	public static void main(String[] args) {
-		//String test=getDateStr(new Date(),"yy");
-		//System.out.println(test);
+	public static void main(String[] args) throws ParseException {
+
+        System.out.println();
 	}
+	
 	public static final String getDateStr(Object date, String pattern) {
 		if (date == null) {
 			return null;
@@ -51,5 +58,50 @@ public final class DateUtils {
 			return new SimpleDateFormat(pattern).format(date);
 		}
 		return new SimpleDateFormat(pattern).format(date);
+	}
+	private void javaTime8() throws ParseException {
+		/********************获取日期和时间***********************/
+		LocalDate date = LocalDate.now();//只存储了日期 比如：2018-12-10
+		LocalTime time = LocalTime.now();//只存储了时间，如：15:52:24.785 (后面的.785表示毫秒值的最后三位使用.withNano(0)可把毫秒值设为0,如LocalTime.now().withNano(0)
+		LocalTime time2 = LocalTime.now().withNano(0);//15:52:24
+		LocalDateTime dateTime=LocalDateTime.now();//存储了日期和时间，比如：2018-12-10T16:03:52.900
+		/******************** Date 转 String***********************/
+		//旧的API 使用Date和SimpleDateFormat
+        SimpleDateFormat simpleDateFormat = 
+                        new SimpleDateFormat("G yyyy年MM月dd号 E a hh时mm分ss秒");
+        String format1 = simpleDateFormat.format(new Date());
+        System.out.println(format1);  //打印: 公元 2017年03月21号 星期二 下午 06时38分20秒
+      //使用jdk1.8 LocalDateTime和DateTimeFormatter
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter pattern = 
+                   DateTimeFormatter.ofPattern("G yyyy年MM月dd号 E a hh时mm分ss秒");
+        String format2 = now.format(pattern);
+        System.out.println(format2); //打印: 公元 2017年03月21号 星期二 下午 06时38分20秒
+        /******************** String 转  Date***********************/
+        //使用Date和SimpleDateFormat
+        SimpleDateFormat simpleDateFormat2 = 
+                  new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date strTodate1 = simpleDateFormat2.parse("2018-12-3 10:15:30");//月份03可以写成3
+        //使用jdk1.8 LocalDateTime和DateTimeFormatter
+        DateTimeFormatter pattern2 = 
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dt = LocalDateTime.parse("2018-12-03 10:15:30",pattern2); //严格按照ISO yyyy-MM-dd验证，03写成3都不行
+        /******************** 日期加减 ***********************/
+        dateTimeAddOrSubtract();
+	}
+	/**
+	 * 日期加减
+	 */
+	private void dateTimeAddOrSubtract() {
+		String dateStr="2018-12-10";
+		LocalDate paramDate = LocalDate.parse(dateStr,DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		paramDate.minusDays(1);//日期减一天
+		paramDate.plusDays(1);//日期加一天
+        LocalDateTime dateTime = LocalDateTime.now();
+        //改变时间后会返回一个新的实例nextYearTime
+        LocalDateTime nextYearTime = dateTime.plusYears(1);//年份增加一年
+        LocalDateTime brefYearTime = dateTime.minusYears(1);//年份减一年
+        LocalDateTime nextDateTime = dateTime.plusDays(1);//日期增加一天
+        LocalDateTime brefDateTime = dateTime.minusDays(1);//日期减一天
 	}
 }
